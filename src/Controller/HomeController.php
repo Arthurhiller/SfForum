675f89917fd229778cf28ebe7fb9b4aebf05422d
Arkang67 <arthur.entrepreneuriat@hotmail.com>
2022-12-19 14:20:57 +0100

    Affichage de toutes les catégories sur la homePage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(): Response
-    {
+    public function index(ManagerRegistry $doctrine): Response
+    {   
+        // Récupère toutes les catégories et les tries par leur nom en DESC
+        $categorie = $doctrine->getRepository(Categorie::class)->findBy([], ['nom' => 'DESC']);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            // Créer un tableau de categories qui prend les valeurs de categorie
+            'categories' => $categorie
         ]);
     }
 }
